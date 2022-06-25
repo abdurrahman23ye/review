9eaabd4a4604e61955ce6c053ebc474f8c874a03
MuratTANC <murattanc@gmail.com>
2022-06-25 18:24:24 +0300

    JDBC

---
+++ JDBC/src/main/java/newnew.java
@@ -0,0 +1,9 @@
+public class newnew extends DatabaseUtilty{
+
+    public static void main(String[] args) {
+        createconnection();
+
+        String query = "select * from ogrenciler";
+
+    }
+}
